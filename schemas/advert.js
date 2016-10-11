564e525e49403e439cc1f7273ad1af8c030a911d
Evgeniy Annenkov <evgeniy.annenkov@farrosoft.com>
2016-10-11 15:26:54 +0300

    added advert schema;

---
+++ schemas/advert.js
@@ -0,0 +1,29 @@
+"use strict";
+const mongoose = require('mongoose');
+
+const advertSchema = new mongoose.Schema({
+    "name": {
+        type: String,
+        required: [ true, 'pet name required' ]
+    },
+    "type" : {
+        type: String,
+        required: [ true, 'pet type required' ]
+    },
+    "gender" : {
+        type: String,
+        required: [ true, 'pet gender required' ]
+    },
+    "age" : {
+        type: String,
+        required: [ true, 'pet age required' ]
+    },
+    "breed" : String,
+    "info" : String,
+    "advertiserID" : {
+        type : mongoose.Schema.Types.ObjectId,
+        required : [ true, 'advertiserID required' ],
+    },
+});
+
+module.exports = mongoose.model('Advert', advertSchema);
