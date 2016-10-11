7014642cf2d92ba8dd1ad521550e0c67a663c3a2
Evgeniy Annenkov <evgeniy.annenkov@farrosoft.com>
2016-10-11 15:12:51 +0300

    Implemented db for advertisers;

--- schemas/user.js
+++ schemas/user.js
@@ -2,18 +2,23 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
+    "oauthID" : Number,
+    "avatar" : String,
     "name": String,
-    "email" : {
-        type : String,
-        validate : {
-            validator : function ( v ) {
-                return /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i.test(
-                    v);
+    "contactInfo" : {
+        "email" : {
+            type : String,
+            validate : {
+                validator : function ( v ) {
+                    return /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i.test(
+                        v);
+                },
+                "message" : '{VALUE} is not a valid email!'
             },
-            "message" : '{VALUE} is not a valid email!'
+            required : [true, 'email required'],
+            unique : true
         },
-        required : [true, 'email required'],
-        unique : true
+        "phone" : String
     },
     "password" : {
         type : String,
