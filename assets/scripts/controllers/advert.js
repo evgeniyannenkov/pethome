77eb3ba8471566f65a7e4c51daef17135dce0a36
dima_goncharuk_farrosoft <dmitry.goncharuk@farrosoft.com>
2016-10-13 18:26:32 +0300

    added new advert controller

--- assets/scripts/controllers/advert.js
+++ assets/scripts/controllers/advert.js
@@ -3,8 +3,24 @@
 function advertControllersInit ( module ) {
 
     module.controller('newAdvertCtrl', [
-        function () {
-
+        "$http",
+        function ( ajax ) {
+            this.advert = {
+                gender : "boy",
+                type : "dog",
+                age : 1
+            };
+            this.create = () => {
+                ajax({
+                    method : "post",
+                    url : "/advert",
+                    data : this.advert
+                }).then(( response ) => {
+                    console.log(response.data);
+                }).catch(( err ) => {
+                    console.log(err);
+                });
+            };
         }
     ])
 }
