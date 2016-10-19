ef538aa5f2ad4703d2516f0fe819e8bbcf9a8bd0
Evgeniy Annenkov <evgeniy.annenkov@farrosoft.com>
2016-10-19 03:41:44 +0300

    added promise; added animation to button;

--- assets/scripts/controllers/auth.js
+++ assets/scripts/controllers/auth.js
@@ -1,22 +1,88 @@
 "use strict";
 function authControllersInit ( module ) {
 
+    const emailRegex = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
+    const passwordRegex = /.*\S.*/;
+
     module.controller('registrationCtrl', [
-        "$http", "authService",
-        function ( $http, authService ) {
+        "$scope", "$http", "$timeout", "authService",
+        function ( $scope, $http, $timeout, authService ) {
+
+            this.emailRegex = emailRegex;
+            this.passwordRegex = passwordRegex;
+            this.type = "registration";
+
+            this.checkForm = (success) => {
+                this.error = !$scope.registrationForm.$valid;
+                this.validClass = $scope.registrationForm.$valid ? "success" : "error";
+            };
 
             this.registration = () => {
-                authService.authenticate("/advertiser", {email : this.email, password : this.password}, "registration");
+                $scope.registrationForm.email.$setTouched();
+                $scope.registrationForm.password.$setTouched();
+
+                if ( $scope.registrationForm.$valid ) {
+
+                    authService.authenticate("/advertiser", {email : this.email, password : this.password})
+                               .then(( response ) => {
+                                   console.log(response);
+                                   if ( response.data.success ) {
+                                       $timeout(() => {
+                                           document.location.reload();
+                                       }, 4000 );
+                                   } else {
+                                       console.log(`${this.type}: failed`);
+                                       console.log(response);
+                                   }
+                               })
+                               .catch(( err ) => {
+                                   if ( !err.data.success ) {
+                                       console.log(err.data.message);
+                                   } else {
+                                       console.log(err);
+                                   }
+                               });
+                } else {
+                    console.log("Registration Form Invalid");
+                }
             };
+
         }
     ]);
 
     module.controller('loginCtrl', [
-        "$http", "authService",
-        function ( $http, authService ) {
+        "$scope", "$http", "authService",
+        function ( $scope, $http, authService ) {
+
+            this.emailRegex = emailRegex;
+            this.passwordRegex = passwordRegex;
+            this.type = "login";
 
             this.login = () => {
-                authService.authenticate("/advertiser/login", {email : this.email, password : this.password}, "login");
+                $scope.loginForm.email.$setTouched();
+                $scope.loginForm.password.$setTouched();
+
+                if ( $scope.loginForm.$valid ) {
+                    authService.authenticate("/advertiser/login", {email : this.email, password : this.password})
+                               .then(( response ) => {
+                                   console.log(response);
+                                   if ( response.data.success ) {
+                                       document.location.reload();
+                                   } else {
+                                       console.log(`${this.type}: failed`);
+                                       console.log(response);
+                                   }
+                               })
+                               .catch(( err ) => {
+                                   if ( !err.data.success ) {
+                                       console.log(err.data.message);
+                                   } else {
+                                       console.log(err);
+                                   }
+                               });
+                } else {
+                    console.log("Login Form Invalid");
+                }
             };
         }
     ]);
--- assets/scripts/services/auth.js
+++ assets/scripts/services/auth.js
@@ -4,24 +4,35 @@ function authServicesInit ( module ) {
     module.factory('authService', [
         "$http", ( $http ) => {
 
-            const authenticate = (url, data, type) => {
-                $http({
-                    method : "post",
-                    url : url,
-                    data : data
-                }).then(( response ) => {
-                    if(response.data.success) {
-                        document.location.reload();
-                    } else {
-                        console.log(`${type}: failed`);
-                        console.log(response);
-                    }
-                }).catch(( err ) => {
-                    if(err.data.success === false) {
-                        console.log(err.data.message);
-                    } else {
-                        console.log(err);
-                    }
+            const authenticate = (url, data) => {
+                //$http({
+                //    method : "post",
+                //    url : url,
+                //    data : data
+                //}).then(( response ) => {
+                //    if(response.data.success) {
+                //        document.location.reload();
+                //    } else {
+                //        console.log(`${type}: failed`);
+                //        console.log(response);
+                //    }
+                //}).catch(( err ) => {
+                //    if(err.data.success === false) {
+                //        console.log(err.data.message);
+                //    } else {
+                //        console.log(err);
+                //    }
+                //});
+                return new Promise((resolve, reject) => {
+                    $http({
+                        method : "post",
+                        url : url,
+                        data : data
+                    }).then(( response ) => {
+                        resolve(response);
+                    }).catch(( err ) => {
+                        reject(err);
+                    });
                 });
             };
 
