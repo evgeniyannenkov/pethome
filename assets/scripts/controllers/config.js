036788f14161a3926eb79d985e984f4b25add2c0
Evgeniy Annenkov <evgeniy.annenkov@farrosoft.com>
2016-10-25 19:21:59 +0300

    change sanitize to escape;

--- assets/scripts/controllers/config.js
+++ assets/scripts/controllers/config.js
@@ -2,7 +2,7 @@
 
 function applicationConfig ( module ) {
     module.config(['$translateProvider', function ($translateProvider){
-        $translateProvider.useSanitizeValueStrategy('sanitize');
+        $translateProvider.useSanitizeValueStrategy('escape');
         $translateProvider.translations('en', {
             BUTTON_LANG_EN : 'english',
             BUTTON_LANG_RU : 'russian'
