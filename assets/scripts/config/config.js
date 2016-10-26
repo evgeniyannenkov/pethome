bcb3993a6987088c77fa73c6513828349e3eada7
Evgeniy Annenkov <evgeniy.annenkov@farrosoft.com>
2016-10-26 20:08:56 +0300

    formatting;

--- assets/scripts/config/config.js
+++ assets/scripts/config/config.js
@@ -1,27 +1,29 @@
 "use strict";
 
 function applicationConfig ( module ) {
-    module.config([ '$translateProvider', function ( $translateProvider ) {
-        const preferred = localStorage["preferred_language"] || "ru";
-        $translateProvider.useSanitizeValueStrategy('escape');
-        $translateProvider.translations('en', {
-            BUTTON_LANG_EN : 'english',
-            BUTTON_LANG_RU : 'russian',
-            SIGN_UP_BTN : "Sign Up",
-            LOGIN_BTN : "Login",
-            HOME_BTN : "Home",
-            PROFILE_BTN : "Profile",
-            LOGOUT_BTN : "Logout",
-        });
-        $translateProvider.translations('ru', {
-            BUTTON_LANG_EN : 'Английский',
-            BUTTON_LANG_RU : 'Русский',
-            SIGN_UP_BTN : "Регистрация",
-            LOGIN_BTN : "Вход",
-            HOME_BTN : "Главная",
-            PROFILE_BTN : "Профиль",
-            LOGOUT_BTN : "Выход",
-        });
-        $translateProvider.preferredLanguage(preferred);
-    } ]);
+    module.config([
+        '$translateProvider', function ( $translateProvider ) {
+            const preferred = localStorage["preferred_language"] || "ru";
+            $translateProvider.useSanitizeValueStrategy('escape');
+            $translateProvider.translations('en', {
+                BUTTON_LANG_EN : 'english',
+                BUTTON_LANG_RU : 'russian',
+                SIGN_UP_BTN : "Sign Up",
+                LOGIN_BTN : "Login",
+                HOME_BTN : "Home",
+                PROFILE_BTN : "Profile",
+                LOGOUT_BTN : "Logout",
+            });
+            $translateProvider.translations('ru', {
+                BUTTON_LANG_EN : 'Английский',
+                BUTTON_LANG_RU : 'Русский',
+                SIGN_UP_BTN : "Регистрация",
+                LOGIN_BTN : "Вход",
+                HOME_BTN : "Главная",
+                PROFILE_BTN : "Профиль",
+                LOGOUT_BTN : "Выход",
+            });
+            $translateProvider.preferredLanguage(preferred);
+        }
+    ]);
 }
