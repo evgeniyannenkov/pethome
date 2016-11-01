b15c075fc1e9a506dc48ffa01194fb2e50f253ab
Evgeniy Annenkov <evgeniy.annenkov@farrosoft.com>
2016-11-01 18:22:20 +0200

    migrated languageSelect directive to language component

--- assets/scripts/directives/languages.js
+++ assets/scripts/directives/languages.js
@@ -3,14 +3,9 @@
 function languagesDirectivesInit ( module ) {
     const templatesFolder = "/assets/templates";
 
-    module.directive('languageSelect', [
-        function () {
-            return {
-                restrict : 'A',
-                templateUrl : `${templatesFolder}/languages-select.html`,
-                controller : "translationCtrl",
-                controllerAs : "translator"
-            };
-        }
-    ]);
+    module.component('language', {
+        templateUrl : `${templatesFolder}/languages-select.html`,
+        controller : "translationCtrl",
+        controllerAs : "translator"
+    });
 }
