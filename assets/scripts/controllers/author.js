4dc3ececda74fd39ea02ae41c96730414f246853
Evgeniy Annenkov <evgeniy.annenkov@farrosoft.com>
2016-11-01 16:37:26 +0200

    migrated author from directives to components;

--- assets/scripts/controllers/author.js
+++ assets/scripts/controllers/author.js
@@ -3,68 +3,80 @@
 function authorControllersInit ( module ) {
 
     module.controller('authorCtrl', [
-        "author", "$scope",
-        function ( author, $scope ) {
-            author.get({ id : $scope.user_id })
-                      .then(( response ) => {
-                          if ( response.data.success ) {
-                              this.info = response.data.author;
-                          }
-                      })
-                      .catch(( response ) => {
-                          console.log(response);
-                      });
+        "author",
+        function ( author ) {
+            author.get({id : this.id})
+                  .then(( response ) => {
+                      if ( response.data.success ) {
+                          this.fields = response.data.author;
+                      }
+                  })
+                  .catch(( response ) => {
+                      console.log(response);
+                  });
         }
     ]);
     module.controller('authorEditCtrl', [
-        "author", "$scope", "notify",
-        function ( author, $scope, notify ) {
+        "author", "notify",
+        function ( author, notify ) {
 
-            this.temporary_data = JSON.parse(JSON.stringify($scope.user));
+            this.temporary_data = JSON.parse(JSON.stringify(this.author));
 
             this.cancel = () => {
-                this.temporary_data = JSON.parse(JSON.stringify($scope.user));
+                this.temporary_data = JSON.parse(JSON.stringify(this.author));
+                if ( this.popup ) {
+                    this.popup.close();
+                }
             };
 
             this.edit = () => {
-                author.update({ id : $scope.user._id, data : this.temporary_data })
-                          .then(( response )=> {
-                              if ( response.data.success ) {
-                                  notify.inform({
-                                      message : `Updated  <i class="fa fa-check" aria-hidden="true"></i>`,
-                                      duration : 1500
-                                  });
-                                  $scope.user = JSON.parse(JSON.stringify(this.temporary_data));
-                                  $scope.popupClose();
+                author.update({id : this.author._id, data : this.temporary_data})
+                      .then(( response )=> {
+                          if ( response.data.success ) {
+                              notify.inform({
+                                  message : `Updated  <i class="fa fa-check" aria-hidden="true"></i>`,
+                                  duration : 1500
+                              });
+                              this.author = JSON.parse(JSON.stringify(this.temporary_data));
+                              if ( this.popup ) {
+                                  this.popup.close();
                               }
-                          })
-                          .catch(( error )=> {
-                              console.log(error);
-                          });
+                          }
+                      })
+                      .catch(( error )=> {
+                          console.log(error);
+                      });
             };
         }
     ]);
 
     module.controller('authorRemoveCtrl', [
-        "author", "$scope", "notify",
-        function ( author, $scope, notify ) {
+        "author", "notify",
+        function ( author, notify ) {
             this.remove = () => {
-                author.remove({ id : $scope.user._id })
-                          .then(( response ) => {
-                              if ( response.data.success ) {
-                                  notify.inform({
-                                      message : `Removed  <i class="fa fa-check" aria-hidden="true"></i>`,
-                                      duration : 1000
-                                  });
-                                  setTimeout(()=> {
-                                      document.location.href = "/";
-                                  }, 1200);
-                              }
-                          })
-                          .catch(( err ) => {
-                              console.log(err);
-                          });
+                author.remove({id : this.author._id})
+                      .then(( response ) => {
+                          if ( response.data.success ) {
+                              notify.inform({
+                                  message : `Removed  <i class="fa fa-check" aria-hidden="true"></i>`,
+                                  duration : 1000
+                              });
+                              setTimeout(()=> {
+                                  document.location.href = "/";
+                              }, 1200);
+                          }
+                      })
+                      .catch(( err ) => {
+                          console.log(err);
+                      });
             };
+
+            this.cancel = () => {
+                if ( this.popup ) {
+                    this.popup.close();
+                }
+            };
+
         }
     ]);
 }
--- assets/scripts/directives/author.js
+++ assets/scripts/directives/author.js
@@ -3,49 +3,38 @@
 function authorDirectivesInit ( module ) {
     const templatesFolder = "/assets/templates";
 
-    module.directive('author', [
-        function () {
-            return {
-                restrict : 'A',
-                templateUrl : `${templatesFolder}/author.html`,
-                scope : {
-                    user_id : "@author",
-                    edit : "=",
-                    remove: "="
-                },
-                controller : "authorCtrl",
-                controllerAs : "author"
-            };
-        }
-    ]);
+    module.component('author', {
+        templateUrl : `${templatesFolder}/author.html`,
+        bindings : {
+            id : "@authorId",
+            edit : "=",
+            remove : "="
+        },
+        controller : "authorCtrl",
+        controllerAs : "author"
+    });
 
-    module.directive('authorEdit', [
-        function () {
-            return {
-                restrict : 'A',
-                templateUrl : `${templatesFolder}/author-edit.html`,
-                scope : {
-                    user : "=authorEdit",
-                    popupClose : "&"
-                },
-                controller : "authorEditCtrl",
-                controllerAs : "editor"
-            };
-        }
-    ]);
+    module.component('authorEdit', {
+        templateUrl : `${templatesFolder}/author-edit.html`,
+        require : {
+            popup: "^^?popup"
+        },
+        bindings : {
+            author : "="
+        },
+        controller : "authorEditCtrl",
+        controllerAs : "editor"
+    });
 
-    module.directive('authorRemove', [
-        function () {
-            return {
-                restrict : 'A',
-                templateUrl : `${templatesFolder}/author-remove.html`,
-                scope : {
-                    user : "=authorRemove",
-                    popupClose : "&popupClose"
-                },
-                controller : "authorRemoveCtrl",
-                controllerAs : "remover"
-            };
-        }
-    ]);
+    module.component('authorRemove', {
+        templateUrl : `${templatesFolder}/author-remove.html`,
+        require : {
+            popup: "^^?popup"
+        },
+        bindings : {
+            author : "="
+        },
+        controller : "authorRemoveCtrl",
+        controllerAs : "remover"
+    });
 }
