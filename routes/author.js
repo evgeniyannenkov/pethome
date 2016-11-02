a91177356e928f9a89450d315d585d94fcfa9310
dima_goncharuk_farrosoft <dmitry.goncharuk@farrosoft.com>
2016-11-02 23:44:02 +0200

    added blocked marks

--- routes/author.js
+++ routes/author.js
@@ -11,16 +11,16 @@ router.get("/:id", ( req, res, next ) => {
         res.redirect('/profile');
     } else {
         Author.findOne({ _id })
-                  .then(( author ) => {
-                      if ( author ) {
-                          res.render("author-single", { author });
-                      } else {
-                          next();
-                      }
-                  })
-                  .catch(( error ) => {
+              .then(( author ) => {
+                  if ( (author && !author.blocked) || (req.user && req.user.is_admin) ) {
+                      res.render("author-single", { author });
+                  } else {
                       next();
-                  });
+                  }
+              })
+              .catch(( error ) => {
+                  next();
+              });
     }
 });
 
