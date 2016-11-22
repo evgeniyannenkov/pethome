deec6ef4b62fc30a9c18da96fbb96ec8febacd5a
Evgeniy Annenkov <evgeniy.annenkov@farrosoft.com>
2016-11-22 02:11:09 +0200

    added oauth config file;

---
+++ config/oauth.js
@@ -0,0 +1,9 @@
+var ids = {
+    facebook: {
+        clientID: '1790274214545803',
+        clientSecret: 'ff8c05c686c4d0c3e272ec70d9587550',
+        callbackURL: 'http://46.101.163.148:3000/auth/facebook/callback'
+    }
+};
+
+module.exports = ids;
