1e8e3e37ec87aad5c50080b4be99dd9914e14de3
dima_goncharuk_farrosoft <dmitry.goncharuk@farrosoft.com>
2016-11-22 20:34:19 +0200

    fixed bug with parallax on mobile

--- assets/scripts/components/images.js
+++ assets/scripts/components/images.js
@@ -32,10 +32,12 @@ function imagesComponentsInit ( module, constants ) {
     module.directive("backgroundBlock", [
         function () {
             return {
-                template : `<div class="background-block {{classes}}" ng-paralax ratio="0.1" style="background-image: url('{{url}}');" ng-transclude></div>`,
+                template : `<div ng-if="parallax" class="background-block {{classes}}" ng-paralax ratio="0.1" style="background-image: url('{{url}}');" ng-transclude></div>
+                            <div ng-if="!parallax" class="background-block {{classes}}" style="background-image: url('{{url}}');" ng-transclude></div>`,
                 scope : {
                     url : "@backgroundBlock",
                     classes : "@",
+                    parrallax : "="
                 },
                 transclude : true,
             };
