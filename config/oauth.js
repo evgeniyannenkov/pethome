847ba73233d2e32b059b450653701226f4de36b8
Evgeniy Annenkov <evgeniy.annenkov@farrosoft.com>
2016-11-22 17:16:08 +0200

    added facebook-login strategy; changed configuration IP;

--- config/oauth.js
+++ config/oauth.js
@@ -2,7 +2,7 @@ var ids = {
     facebook: {
         clientID: '1790274214545803',
         clientSecret: 'ff8c05c686c4d0c3e272ec70d9587550',
-        callbackURL: 'http://46.101.163.148:3000/auth/facebook/callback'
+        callbackURL: 'http://138.68.95.45:3000/auth/facebook/callback'
     }
 };
 
