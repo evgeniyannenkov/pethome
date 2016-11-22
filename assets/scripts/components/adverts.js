ffa8b119fd09b37fe56612ca81c2935209ff21e2
dima_goncharuk_farrosoft <dmitry.goncharuk@farrosoft.com>
2016-11-22 23:43:00 +0200

    replaced template by templateUrl

--- assets/scripts/components/adverts.js
+++ assets/scripts/components/adverts.js
@@ -25,41 +25,7 @@ function advertComponentsInit ( module, constants ) {
     });
 
     module.component('advert', {
-        // templateUrl : `${constants.templatesFolder}/advert.html`,
-        template : `<div class="advert__head">
-                      <div ng-if="advert.fields.title &amp;&amp; !advert.hide.title" class="advert__field advert__field--title"><a href="/advert/{{advert.fields._id}}">{{advert.fields.title}}</a></div>
-                      <div background-image="{{advert.fields.mainImage}}" type="{{advert.fields.type}}" class="advert__field advert__field--image"><a href="/advert/{{advert.fields._id}}"></a></div>
-                      <div class="advert__field advert__field--time">{{advert.fields.publicationDate | translate : 'time'}}</div>
-                      <div class="advert__field advert__field--date">{{advert.fields.publicationDate | translate : 'date'}}</div>
-                    </div>
-                    <div class="advert__content">
-                      <div ng-if="!advert.hide.name" class="advert__field">
-                        <div class="advert__field-name">{{'name' | translate}}</div>
-                        <div class="advert__field-content">{{advert.fields.name}}</div>
-                      </div>
-                      <div ng-if="!advert.hide.type" class="advert__field">
-                        <div class="advert__field-name">{{'type' | translate}}</div>
-                        <div class="advert__field-content">{{advert.fields.type | translate}}</div>
-                      </div>
-                      <div ng-if="!advert.hide.gender" class="advert__field">
-                        <div class="advert__field-name">{{'gender' | translate}}</div>
-                        <div class="advert__field-content">{{advert.fields.gender | translate}}</div>
-                      </div>
-                      <div ng-if="!advert.hide.age" class="advert__field">
-                        <div class="advert__field-name">{{'age' | translate}}</div>
-                        <div class="advert__field-content">{{advert.fields.age || 'Not specified' | translate}}</div>
-                      </div>
-                      <div ng-if="!advert.hide.breed" class="advert__field">
-                        <div class="advert__field-name">{{'breed' | translate}}</div>
-                        <div class="advert__field-content">{{advert.fields.breed || 'Not specified' | translate}}</div>
-                      </div>
-                      <div ng-if="!advert.hide.author" class="advert__field advert__field--author"><span class="advert__field-name">{{'Author' | translate}}</span><a href="/author/{{advert.author._id}}" class="advert__field-content"> {{advert.author.name || 'Go to' | translate}}</a></div>
-                      <div ng-if="!advert.hide.info" class="advert__field advert__field--info">
-                        <div class="advert__field-name advert__field-name--small">{{'info' | translate}}</div>
-                        <div class="advert__field-content">{{advert.fields.info || 'Not specified' | translate | excerpt : 10}}</div>
-                        <a type="button" href="/advert/{{advert.fields._id}}" class="button button--read_more button--blue button--invert_hover"><span class="button__title">{{'Read more' | translate}}</span></a>
-                      </div>
-                    </div>`,
+        templateUrl : `${constants.templatesFolder}/advert.html`,
         bindings : {
             fields : "=",
             author : "=",
@@ -72,7 +38,7 @@ function advertComponentsInit ( module, constants ) {
         require : {
             currentUser : "^^currentUser"
         },
-        templateUrl : "http://138.68.95.45:3000/advert/58348ae7ad62c30459a5de7c",
+        templateUrl : `${constants.templatesFolder}/advert-single.html`,
         bindings : {
             id : "@advertId",
             reviewEnabled : "=enableReview",
