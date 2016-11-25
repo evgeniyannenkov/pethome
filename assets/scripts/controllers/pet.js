12cf33d247e043bc7e19f6c758efb0f1377dda28
dima_goncharuk_farrosoft <dmitry.goncharuk@farrosoft.com>
2016-11-25 15:27:18 +0200

    setting page to 1 after changin perPage limit

--- assets/scripts/controllers/pet.js
+++ assets/scripts/controllers/pet.js
@@ -283,6 +283,7 @@ function petControllersInit ( module ) {
             };
 
             this.changeLimit = () => {
+                this.page = 1;
                 this.feedData.limit = this.perPage;
                 this.getFeed();
             };
